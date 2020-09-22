da4fa192479b2d3e202c7eecc378c72cd04624e8
Joseph Nagy <jn135@duke.edu>
2020-09-22 17:58:34 -0400

    implimented url json

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import json
+import os.path
 
 app = Flask(__name__)
 
@@ -6,6 +8,24 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route('/about')
-def about():
-    return 'This is a url shortner'
+
+@app.route('/your-url', methods=['GET', 'POST'])
+def your_url():
+    if request.method == 'POST':
+        urls = {} #initialize url dict (key=shortened_url) : (value=real_url)
+
+        #check if json file exists
+        if os.path.exists('urls.json'):
+            with open('urls.json') as urls_file:
+                urls = json.load(urls_file)
+
+        #if shortened_url has already been used, return to homepage 
+        if request.form['code'] in urls.keys():
+            return redirect(url_for('home')) #redirect user back to homepage
+
+        urls[request.form['code']] = {'url':request.form['url']}
+        with open('urls.json','w') as url_file:
+            json.dump(urls, url_file)
+        return render_template('your_url.html', code=request.form['code'])
+    else:
+        return redirect(url_for('home')) #redirect user back to homepage
